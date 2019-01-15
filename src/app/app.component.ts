d53994b3753dfa85b505865d64308884ba10be12
wheeljs <fang.tian@85ido.com>
2019-01-15 14:40:43 +0800

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { NzPopoverDirective } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'nz-popover-viewchild';
+
+  @ViewChild(NzPopoverDirective)
+  popover: NzPopoverDirective;
+
+  ngAfterViewInit(): void {
+    console.log(this.popover);
+  }
 }
